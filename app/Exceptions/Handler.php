0b0b60f8d5405075cf676910833989f01df27294
kai0310 <kn0310@outlook.jp>
2021-09-11 03:04:48 +0900

    Revert "fix: set error handling"
    This reverts commit 5ffdbab3d0cfa1e7bd4ba69462129b4ddfd9ad45.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Inertia\Inertia;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -38,24 +37,5 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
-    }
-
-    public function render($request, Throwable $e)
-    {
-        $response = parent::render($request, $e);
-
-        if (!app()->environment(['local', 'testing']) && in_array($response->status(), [500, 503, 404, 403])) {
-            return Inertia::render('Error', ['status' => $response->status()])
-                ->toResponse($request)
-                ->setStatusCode($response->status());
-        }
-
-        if ($response->status() === 419) {
-            return back()->with([
-                'message' => 'The page expired, please try again.',
-            ]);
-        }
-
-        return $response;
     }
 }
